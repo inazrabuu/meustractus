67a769c5c2c78f2d608e551ef037a93c1700d0ba
Arman Rizal <inazrabuu@gmail.com>
2015-08-04 14:58:36 +0700

    regular sync

--- python/locust/mm.py
+++ python/locust/mm.py
@@ -35,31 +35,31 @@ class MMTaskSet(TaskSet):
 	def browsing(self):
 		# Go to a category
 		categoryPage = self.client.get(random.choice(self.categories))
-		category = BeautifulSoup(categoryPage.content, "html.parser")
-		urlPdp = random.choice(category("div", "item-product-list")).find('a')#
-		if urlPdp is not None:
-			# Go to a product detail page
-			pdpPage = self.client.get(urlPdp.get('href'))
-			#if pdpPage is not None:
-#				pdp =  BeautifulSoup(pdpPage.content, "html.parser")
-#				form = pdp("form", "commerce-add-to-cart")
-#				inputs = form[0].find_all('input')
-#				param = {
-#					"_triggering_element_value" : "Beli Aja",
-#					"quantity" : 1
-#				}
-#				for inp in inputs:
-#					if inp.get('name') == 'product_id' or inp.get('name') == 'form_id' or inp.get('name') == 'form_build_id':
-#						param[inp.get('name')] = inp.get('value')#
+		if categoryPage is not None:
+			category = BeautifulSoup(categoryPage.content, "html.parser")
+			urlPdp = random.choice(category("div", "item-product-list")).find('a')#
+#			if urlPdp is not None:
+#				# Go to a product detail page
+#				pdpPage = self.client.get(urlPdp.get('href'))
+#				if pdpPage is not None:
+#					pdp =  BeautifulSoup(pdpPage.content, "html.parser")
+#					form = pdp("form", "commerce-add-to-cart")
+#					inputs = form[0].find_all('input')
+#					param = {
+#						"_triggering_element_value" : "Beli Aja",
+#						"quantity" : 1
+#					}
+#					for inp in inputs:
+#						if inp.get('name') == 'product_id' or inp.get('name') == 'form_id' or inp.get('name') == 'form_build_id':
+#							param[inp.get('name')] = inp.get('value')#
+#					# Put an item into shopping cart
+#					atcResponse = self.client.post(self.endpoints['add_to_cart'], param)
+#					
+#					# View the cart
+#					cartResponse = self.client.get(self.endpoints['cart'])
+#					print cartResponse.content
 
-#				# Put an item into shopping cart
-#				atcResponse = self.client.post(self.endpoints['add_to_cart'], param)
-#				
-#				# View the cart
-#				cartResponse = self.client.get(self.endpoints['cart'])
-#				print cartResponse.content
-
-#	def login(self):
+	def login(self):
 
 	
 class MMUser(HttpLocust):
