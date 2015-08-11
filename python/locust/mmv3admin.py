110f024e065f0f075b11a73b00b3c4747ce903c0
Arman Rizal <inazrabuu@gmail.com>
2015-08-11 13:26:27 +0700

    adding mmv3admin

---
+++ python/locust/mmv3admin.py
@@ -0,0 +1,36 @@
+from locust import HttpLocust, TaskSet, task
+from bs4 import BeautifulSoup
+
+class MMAdminTask(TaskSet):
+	endpoints = {
+		'home' : '/',
+		'login' : '/auth/auth/login',
+		'logout': '/auth/logout'
+	}
+
+	#login > logout
+	@task(1)
+	def scenario1(self):
+		homeResponse = self.client.get(self.endpoints['home'])
+		
+		home = BeautifulSoup(homeResponse.content, 'html.parser')
+
+		param = {
+			'email' : 'admin@mataharimall.com',
+			'password' : '123456',
+		}
+
+		for inpute in home.find_all('input'):
+			if (inpute.get('type') == 'hidden'):
+				param[inpute.get('name')] = inpute.get('value')
+
+		#login
+		loginResponse = self.client.post(self.endpoints['home'], param)
+
+		#logout
+		logoutResponse = self.client.get(self.endpoints['logout'])		
+
+class MMAdminUser(HttpLocust):
+	task_set = MMAdminTask
+	min_wait = 5000
+	max_wait = 9000
